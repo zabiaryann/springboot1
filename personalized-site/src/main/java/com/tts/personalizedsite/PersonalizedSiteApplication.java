5d74605adf8f283c07a9562c67850ac2fe2104e4
Zabi Aryan <zabiullaharyan@yahoo.com>
2023-08-22 17:56:00 -0700

    first commit

---
+++ personalized-site/src/main/java/com/tts/personalizedsite/PersonalizedSiteApplication.java
@@ -0,0 +1,13 @@
+package com.tts.personalizedsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizedSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizedSiteApplication.class, args);
+	}
+
+}
---
+++ personalized-site/src/test/java/com/tts/personalizedsite/PersonalizedSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.personalizedsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizedSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
